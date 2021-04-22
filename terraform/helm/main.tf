d91a2c623ae0be2f16ea9176ba1cae505887275e
Xinyu Zhang <zhang.xinyu3@husky.neu.edu>
2021-04-22 00:53:09 -0400

    Add dns record for all dashboards that need to be exposed

--- terraform/helm/main.tf
+++ terraform/helm/main.tf
@@ -84,13 +84,109 @@ data "google_dns_managed_zone" "gke_prod_zone" {
   name        = var.zone_name
 }
 
+locals {
+  ingressgateway_ip = data.kubernetes_service.istio_ingressgateway.status.0.load_balancer.0.ingress.0.ip
+}
+
 resource "google_dns_record_set" "gke_prod_resource_recordset" {
   name         = var.dns_record_name
   type         = "A"
   ttl          = 60
 
   managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
-  rrdatas      = [data.kubernetes_service.istio_ingressgateway.status.0.load_balancer.0.ingress.0.ip]
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+resource "google_dns_record_set" "grafana_gke_prod_resource_recordset" {
+  name         = join(".", ["grafana", var.dns_record_name])
+  type         = "A"
+  ttl          = 60
+
+  managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+resource "google_dns_record_set" "kibana_gke_prod_resource_recordset" {
+  name         = join(".", ["kibana", var.dns_record_name])
+  type         = "A"
+  ttl          = 60
+
+  managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+resource "google_dns_record_set" "kiali_gke_prod_resource_recordset" {
+  name         = join(".", ["kiali", var.dns_record_name])
+  type         = "A"
+  ttl          = 60
+
+  managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+resource "google_dns_record_set" "tracing_gke_prod_resource_recordset" {
+  name         = join(".", ["tracing", var.dns_record_name])
+  type         = "A"
+  ttl          = 60
+
+  managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+resource "google_dns_record_set" "istio_grafana_gke_prod_resource_recordset" {
+  name         = join(".", ["istio-grafana", var.dns_record_name])
+  type         = "A"
+  ttl          = 60
+
+  managed_zone = data.google_dns_managed_zone.gke_prod_zone.name
+  rrdatas      = [local.ingressgateway_ip]
+}
+
+# cert-manager crds
+resource "null_resource" "cert_manager_crds" {
+  provisioner "local-exec" {
+    command = "kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.crds.yaml"
+  }
+}
+
+# cert-manager
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+
+  repository = "https://charts.jetstack.io"
+  chart      = "cert-manager"
+  namespace = "cert-manager"
+  create_namespace = true
+  version = "v0.14.0"
+}
+
+# letsencrypt-staging/letsencrypt-prod
+data "kubectl_file_documents" "manifests_letsencrypt" {
+    content = file("./manifests/letsencrypt-staging.yaml")
+    # content = file("./manifests/letsencrypt-prod.yaml")
+}
+
+resource "kubectl_manifest" "letsencrypt" {
+  count     = length(data.kubectl_file_documents.manifests_letsencrypt.documents)
+  yaml_body = element(data.kubectl_file_documents.manifests_letsencrypt.documents, count.index)
+
+  depends_on = [
+    helm_release.cert_manager
+  ]
+}
+
+# Issue a certficate
+data "kubectl_file_documents" "manifests_certificate" {
+    content = file("./manifests/certificate.yaml")
+}
+
+resource "kubectl_manifest" "certificate" {
+  count     = length(data.kubectl_file_documents.manifests_certificate.documents)
+  yaml_body = element(data.kubectl_file_documents.manifests_certificate.documents, count.index)
+
+  depends_on = [
+    kubectl_manifest.letsencrypt
+  ]
 }
 
 # kafka
@@ -140,80 +236,6 @@ resource "helm_release" "elastic_search" {
   repository = "https://helm.elastic.co"
   chart      = "elasticsearch"
 }
-
-# # ingress-controller
-# resource "helm_release" "nginx_ingress" {
-#   name       = "nginx-ingress-controller"
-
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "nginx-ingress-controller"
-
-#   set {
-#     name  = "controller.service.type"
-#     value = "LoadBalancer"
-#   }
-#   set {
-#     name  = "controller.service.loadBalancerIP"
-#     value = "35.227.22.228"
-#   }
-# }
-
-# cert-manager crds
-resource "null_resource" "cert_manager_crds" {
-  provisioner "local-exec" {
-    command = "kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager.crds.yaml"
-  }
-}
-
-# cert-manager
-resource "helm_release" "cert_manager" {
-  name       = "cert-manager"
-
-  repository = "https://charts.jetstack.io"
-  chart      = "cert-manager"
-  namespace = "cert-manager"
-  create_namespace = true
-  version = "v0.14.0"
-}
-
-# letsencrypt-staging/letsencrypt-prod
-data "kubectl_file_documents" "manifests_letsencrypt" {
-    content = file("./manifests/letsencrypt-staging.yaml")
-    # content = file("./manifests/letsencrypt-prod.yaml")
-}
-
-resource "kubectl_manifest" "letsencrypt" {
-  count     = length(data.kubectl_file_documents.manifests_letsencrypt.documents)
-  yaml_body = element(data.kubectl_file_documents.manifests_letsencrypt.documents, count.index)
-
-  depends_on = [
-    helm_release.cert_manager
-  ]
-}
-
-# Issue a certficate
-data "kubectl_file_documents" "manifests_certificate" {
-    content = file("./manifests/certificate.yaml")
-}
-
-resource "kubectl_manifest" "certificate" {
-  count     = length(data.kubectl_file_documents.manifests_certificate.documents)
-  yaml_body = element(data.kubectl_file_documents.manifests_certificate.documents, count.index)
-
-  depends_on = [
-    kubectl_manifest.letsencrypt
-  ]
-}
-
-# # multi-app-ingress
-# data "kubectl_filename_list" "manifests_ingress" {
-#     pattern = "./manifests/multi-app-ingress.yaml"
-# }
-
-# resource "kubectl_manifest" "ingress" {
-#     count     = length(data.kubectl_filename_list.manifests_ingress.matches)
-#     yaml_body = file(element(data.kubectl_filename_list.manifests_ingress.matches, count.index))
-# }
 
 # metrics-server
 data "kubectl_filename_list" "manifests_metrics_server" {
